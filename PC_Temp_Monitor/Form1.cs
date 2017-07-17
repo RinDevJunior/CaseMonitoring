489359853995a40c22f75f2c899049ef71a64182
rinnv <rinnv.spkt@gmail.com>
2017-07-17 22:03:17 +0700

    init project

--- PC_Temp_Monitor/Form1.cs
+++ PC_Temp_Monitor/Form1.cs
@@ -1,14 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Globalization;
 using System.IO.Ports;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Timers;
 using System.Windows.Forms;
 using OpenHardwareMonitor.Hardware;
 using Timer = System.Windows.Forms.Timer;
