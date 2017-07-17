bdf49dfc9fa3a60d725556c86d0e63ebdeb6c83d
rinnv <rinnv.spkt@gmail.com>
2017-07-17 21:56:19 +0700

    Add project files.

---
+++ PC_Temp_Monitor/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace PC_Temp_Monitor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cpuTemp = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.gpuTemp = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(849, 214);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "CPU:";
+            // 
+            // cpuTemp
+            // 
+            this.cpuTemp.AutoSize = true;
+            this.cpuTemp.Location = new System.Drawing.Point(75, 24);
+            this.cpuTemp.Name = "cpuTemp";
+            this.cpuTemp.Size = new System.Drawing.Size(52, 13);
+            this.cpuTemp.TabIndex = 2;
+            this.cpuTemp.Text = "cpuTemp";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(31, 55);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "GPU:";
+            // 
+            // gpuTemp
+            // 
+            this.gpuTemp.AutoSize = true;
+            this.gpuTemp.Location = new System.Drawing.Point(75, 55);
+            this.gpuTemp.Name = "gpuTemp";
+            this.gpuTemp.Size = new System.Drawing.Size(52, 13);
+            this.gpuTemp.TabIndex = 4;
+            this.gpuTemp.Text = "gpuTemp";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(978, 261);
+            this.Controls.Add(this.gpuTemp);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cpuTemp);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label cpuTemp;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label gpuTemp;
+    }
+}
+
---
+++ PC_Temp_Monitor/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+using OpenHardwareMonitor.Hardware;
+using Timer = System.Windows.Forms.Timer;
+
+
+namespace PC_Temp_Monitor
+{
+    public partial class Form1 : Form
+    {
+        SerialPort port = new SerialPort();
+
+        Computer _computer = new Computer()
+        {
+            GPUEnabled = true,
+            CPUEnabled = true
+        };
+
+        static float value1;
+        static float value2;
+
+        public Form1()
+        {
+            InitializeComponent();
+            _computer.Open();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var timer = new Timer();
+            timer.Tick += Status; // Everytime timer ticks, timer_Tick will be called
+            timer.Interval = (1000) * (1);              // Timer will tick evert second
+            timer.Enabled = true;                       // Enable the timer
+            timer.Start();
+        }
+
+        private void Status(object sender, EventArgs e)
+        {
+            foreach (var hardwadre in _computer.Hardware)
+            {
+                if (hardwadre.HardwareType == HardwareType.GpuNvidia)
+                {
+                    hardwadre.Update();
+                    foreach (var sensor in hardwadre.Sensors)
+                        if (sensor.SensorType == SensorType.Temperature)
+                        {
+                            value1 = sensor.Value.GetValueOrDefault();
+                            gpuTemp.Text = value1.ToString(CultureInfo.InvariantCulture);
+                        }
+                }
+                if (hardwadre.HardwareType == HardwareType.CPU)
+                {
+                    hardwadre.Update();
+                    foreach (var sensor in hardwadre.Sensors)
+                        if (sensor.SensorType == SensorType.Temperature)
+                        {
+                            value2 = sensor.Value.GetValueOrDefault();
+                            cpuTemp.Text = value2.ToString(CultureInfo.InvariantCulture);
+                        }
+                }
+            }
+        }
+
+    }
+}
